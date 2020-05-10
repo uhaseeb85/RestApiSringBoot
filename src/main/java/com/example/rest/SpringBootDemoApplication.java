f296fa7ec63fcd2a291a272ccab29f7ce4003f78
uhaseeb85 <uhaseeb85@gmail.com>
2020-05-10 15:20:35 +0530

    Added bucket4j approach for throttling.

--- src/main/java/com/example/rest/SpringBootDemoApplication.java
+++ src/main/java/com/example/rest/SpringBootDemoApplication.java
@@ -1,12 +1,31 @@
 package com.example.rest;
 
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication 
-public class SpringBootDemoApplication {
+import com.example.throttling.filter.RateLimitInterceptor;
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
-    }
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+
+@SpringBootApplication
+public class SpringBootDemoApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		Refill refill = Refill.greedy(10, Duration.ofMinutes(1));
+		Bandwidth limit = Bandwidth.classic(10, refill).withInitialTokens(1);
+		Bucket bucket = Bucket4j.builder().addLimit(limit).build();
+		registry.addInterceptor(new RateLimitInterceptor(bucket, 1)).addPathPatterns("/employees/bucket4j");
+	}
 }
--- src/main/java/com/example/rest/controller/EmployeeController.java
+++ src/main/java/com/example/rest/controller/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.example.rest.controller;
 
 import java.net.URI;
+import java.time.Duration;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +21,23 @@ import com.example.rest.model.Employee;
 import com.example.rest.model.Employees;
 import com.google.common.util.concurrent.RateLimiter;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController 
 {
+	private final Bucket bucket;
 	
-	final RateLimiter rateLimiter = RateLimiter.create(0.25);
+	private final RateLimiter rateLimiter = RateLimiter.create(0.25);
+	
+	public EmployeeController() {
+		long capacity = 10;
+		Bandwidth limit = Bandwidth.simple(capacity, Duration.ofMinutes(1));
+	    this.bucket = Bucket4j.builder().addLimit(limit).build();
+	}
 	
     @Autowired
     private EmployeeDAO employeeDao;
@@ -35,9 +50,18 @@ public class EmployeeController
     }
     
     @GetMapping(path="/weddini", produces = "application/json")
-    public Employees getEmployeesWeddini() 
+    public ResponseEntity<Employees> getEmployeesWeddini(HttpServletResponse response) 
     {
-    	rateLimiter.acquire();
+    	 if (bucket.tryConsume(1)) {
+    		 Employees body = employeeDao.getAllEmployees();
+    		 return ResponseEntity.ok().body(body);
+    	 }
+    	 return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+    }
+    
+    @GetMapping(path="/bucket4j", produces = "application/json")
+    public Employees getEmployeesBucket4j() 
+    {
         return employeeDao.getAllEmployees();
     }
     
