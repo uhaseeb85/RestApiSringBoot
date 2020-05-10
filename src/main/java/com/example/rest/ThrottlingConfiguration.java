ca785dd9ff3bf2541fedeee3404ef6dc36c64836
uhaseeb85 <uhaseeb85@gmail.com>
2020-05-10 15:30:58 +0530

    Refactoring.

---
+++ src/main/java/com/example/rest/ThrottlingConfiguration.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.example.rest;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.throttling.filter.RateLimitInterceptor;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+
+/**
+ * The Class ThrottlingConfiguration.
+ *
+ * @author Haseeb
+ */
+@Configuration
+public class ThrottlingConfiguration implements WebMvcConfigurer {
+	
+	private static final long NUMBER_OF_REQUESTS=10;
+	
+	/**
+	 * Adds the interceptors.
+	 *
+	 * @param registry the registry
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		Bandwidth limit = Bandwidth.simple(NUMBER_OF_REQUESTS, Duration.ofMinutes(1));
+		Bucket bucket = Bucket4j.builder().addLimit(limit).build();
+		registry.addInterceptor(new RateLimitInterceptor(bucket, 1)).addPathPatterns("/employees/bucket4j");
+	}
+
+}
