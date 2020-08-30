421764f6c8deffbff84090b1b0f30e49802bdca9
uhaseeb85 <uhaseeb85@gmail.com>
2020-08-30 20:26:17 +0530

    Added spring boot scheduling.

--- src/main/java/com/example/rest/SpringBootDemoApplication.java
+++ src/main/java/com/example/rest/SpringBootDemoApplication.java
@@ -2,10 +2,10 @@ package com.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
+@EnableScheduling
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
